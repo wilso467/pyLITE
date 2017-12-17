2311e99155070c6b50004acc8db0dc39cf9201dd
root <root@wuklab19-vm01.localdomain>
2017-12-17 06:06:18 -0500

    Changed shared object file source nonsense.

--- lite-userspace/test.py
+++ lite-userspace/test.py
@@ -2,13 +2,14 @@
 import ctypes
 
 
+#testlib = ctypes.CDLL('./test.so')
 
-
-testlib = ctypes.CDLL('./test.so')
+lite_lib = ctypes.CDLL('./lite-lib.so')
 
 def join_cluster():
 
-    testlib.join()
+    #testlib.join()
+    print("This node id is", lite_lib.userspace_liteapi_get_node_id())
 
 
 
@@ -16,8 +17,9 @@ def join_cluster():
 
 def get_LITE_cluster_info():
 
-    print("This node has id", testlib.get_own_cluster_id())
-    print("This cluster has "+  str(testlib.get_cluster_size())+  " nodes.")
+    #print("This node has id", testlib.get_own_cluster_id())
+    #print("This cluster has "+  str(testlib.get_cluster_size())+  " nodes.")
+    print("Duppity do")
 
 
 
@@ -26,6 +28,6 @@ def get_LITE_cluster_info():
 # a function that receives an array and prints it out
 
 
-# 
+#
 join_cluster()
 get_LITE_cluster_info()
